65472148d13edda103bd346d40359d7df81230e0
Pablo Porto <paulo.assis@ccc.ufcg.edu.br>
2021-07-24 07:11:56 -0300

    Initial commit

---
+++ src/main/java/com/bikefactory/BikeFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.bikefactory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeFactoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BikeFactoryApplication.class, args);
+    }
+
+}
